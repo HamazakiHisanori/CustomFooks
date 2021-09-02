3bfe1cdeef8f5e1f8167644414a41ee044c5510d
HamazakiHisanori <81221219+HamazakiHisanori@users.noreply.github.com>
2021-09-02 16:42:06 +0900

    no Custom-Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,52 @@
 import "./styles.css";
+import axios from "axios";
+
+import { UserCard } from "./components/UserCard";
+import { User } from "./types/api/user";
+import { useState } from "react";
+import { UserProfile } from "./types/UserProfile";
 
 export default function App() {
+  const [userProfiles, setUerProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  const onClickGetData = () => {
+    setLoading(true);
+    setError(false);
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUerProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
   return (
     <div className="App">
-      <h1>Custom Fooks</h1>
+      <button onClick={onClickGetData}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>データ取得中...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <UserCard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
